87550dc53034d7051e119922230af76b86ca19cc
Abraham <abrahamshimekt4096@gmail.com>
2022-04-23 21:11:03 +0300

    draw again button added inorder to draw the fuctions one more time witout rerun the program

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from tkinter import *
-import pygame
-from OpenGL.GL import *
-from OpenGL.GLU import *
-from pygame.locals import *
-import numpy as np
+from tkinter import *  # from tkinter module import all classes
+import pygame  # import a pygame class
+from OpenGL.GL import *  # from python opengl.gl module import all classes
+from OpenGL.GLU import *  # from opengl.glu module import all classes
+from pygame.locals import *  # from pygame.locals import all class
+import numpy as np  # import a python numerical library
+
+window = Tk()  # tkinter window
+window.geometry("400x300")  # dimension for tkinter window
+window.title("grapher")  # name for tkinter window
+
+""" Init function initialize the pygame display window with width, height and colors
+"""
 
 
 def init():
@@ -12,6 +19,10 @@ def init():
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
     glClearColor(0.0, 0.0, 0.0, 1.0)
     gluOrtho2D(-10.0, 10.0, -10.0, 10.0)
+
+
+""" Draw_coordinate function draws the x,y coordinate when the pygame window displayed by the draw function
+"""
 
 
 def draw_coordinate():
@@ -24,11 +35,19 @@ def draw_coordinate():
     glEnd()
 
 
+""" The draw_quadratic function draw a polynomial function with degree of 2 turning point at (0,0) and concave upward 
+"""
+
+
 def draw_quadratic():
     x = np.linspace(-10, 10, 1000)
     y = np.power(x, 2)
     for a, b in zip(x, y):
         glVertex2f(a, b)
+
+
+""" The draw_quadratic function draw a polynomial function with degree of 3 turning point at (0,0) and monotonically increasing
+"""
 
 
 def draw_cubic():
@@ -38,25 +57,41 @@ def draw_cubic():
         glVertex2f(a, b)
 
 
+""" The draw_log function draws the function with base e which is a natural logarithmic function
+"""
+
+
 def draw_log():
-    x = np.linspace(-1, 1, 100)
-    y = np.log2(x)
+    x = np.linspace(0, 10, 1000)
+    y = np.log(x)
     for a, b in zip(x, y):
         glVertex2f(a, b)
 
 
+""" Draw_sin function draws a sin trigonometric function that has an amplitude of 1
+"""
+
+
 def draw_sin():
-    x = np.linspace(-10, 10, 1000)
+    x = np.linspace(-360, 360, 1000)
     y = np.sin(x)
     for a, b in zip(x, y):
         glVertex2f(a, b)
 
 
+"""Draw_cosine function draws a cosine trigonometric function that has an amplitude of 1
+"""
+
+
 def draw_cosine():
-    x = np.linspace(-10, 10, 1000)
+    x = np.linspace(-360, 360, 1000)
     y = np.cos(x)
     for a, b in zip(x, y):
         glVertex2f(a, b)
+
+
+"""Draw_power function draws a power mathematical function 2^x
+"""
 
 
 def draw_power():
@@ -64,6 +99,17 @@ def draw_power():
     y = np.power(2, x)
     for a, b in zip(x, y):
         glVertex2f(a, b)
+
+
+var0 = IntVar()  # integer variable for quadratic
+var1 = IntVar()  # integer variable for cubic
+var2 = IntVar()  # integer variable for sin
+var3 = IntVar()  # integer variable for function
+var4 = IntVar()  # integer variable for log
+var5 = IntVar()  # integer variable for exponential
+
+""" This function draws functions based on the if condition by getting the var value from check buttons
+"""
 
 
 def draw():
@@ -107,32 +153,32 @@ def draw():
     glFlush()
 
 
-window = Tk()
-window.geometry("550x350")
-intro = Label(window, text="Welcome to my project", font=40, )
-choice = Label(window, text="choose functions", font=5)
+"""draw_again restart the pygame window """
 
-var0 = IntVar()
-var1 = IntVar()
-var2 = IntVar()
-var3 = IntVar()
-var4 = IntVar()
-var5 = IntVar()
-quadratic = Checkbutton(window, text=" X^2", variable=var0, onvalue=True, offvalue=False, bg="orange")
-cubic = Checkbutton(window, text=" X^3", variable=var1, onvalue=True, offvalue=False, bg="orange")
-sin = Checkbutton(window, text="sin(x)", variable=var2, onvalue=True, offvalue=False, bg="orange")
-cosine = Checkbutton(window, text="cos(x)", variable=var3, onvalue=True, offvalue=False, bg="orange")
-log = Checkbutton(window, text="log(x)", variable=var4, onvalue=True, offvalue=False, bg="orange")
-exponential = Checkbutton(window, text=" 2^x", variable=var5, onvalue=True, offvalue=False, bg="orange")
 
 def draw_again():
-    pygame.display.flip()
-    var0.set(False)
-    var1.set(False)
-    var2.set(False)
-    var3.set(False)
-    var4.set(False)
-    var5.set(False)
+    var0.set(False)  # off the var0 check button
+    var1.set(False)  # off the var1 check button
+    var2.set(False)  # off the var2 check button
+    var3.set(False)  # off the var3 check button
+    var4.set(False)  # off the var4 check button
+    var5.set(False)  # off the var5 check button
+    pygame.quit()  # close the current window
+    init()  # initialize the window to be drawn
+    while True:  # display the pygame window until the pygame quit function called
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        pygame.display.flip()
+        pygame.time.wait(10)
+        window.update()  # update tkinter window
+
+
+""" main display the pygame window with the specified mathematical function
+"""
+
+
 def main():
     init()
     while True:
@@ -146,10 +192,24 @@ def main():
         window.update()
 
 
-again = Button(window, text="draw again", command=draw_again,bg="red")
-draw_button = Button(window, text="Draw", bg='teal', command=main)
-intro.grid(row=0, column=1, columnspan=6, padx=40, pady=20)
-choice.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
+intro = Label(window, text="Welcome to my project", font=30, fg="teal")  # intro label text
+choice = Label(window, text="choose functions", font=3, justify="center")  # a prompt label text
+
+"""tkinter check buttons for each mathematical function  """
+quadratic = Checkbutton(window, text=" X^2", variable=var0, onvalue=True, offvalue=False, bg="orange", width=5)
+cubic = Checkbutton(window, text=" X^3", variable=var1, onvalue=True, offvalue=False, bg="orange", width=5)
+sin = Checkbutton(window, text="sin(x)", variable=var2, onvalue=True, offvalue=False, bg="orange", width=5)
+cosine = Checkbutton(window, text="cos(x)", variable=var3, onvalue=True, offvalue=False, bg="orange", width=5)
+log = Checkbutton(window, text="log(x)", variable=var4, onvalue=True, offvalue=False, bg="orange", width=5)
+exponential = Checkbutton(window, text=" 2^x", variable=var5, onvalue=True, offvalue=False, bg="orange", width=5)
+
+# draw button and draw again button
+draw_button = Button(window, text="Draw", bg='teal', command=main, width=8)
+draw_again_button = Button(window, text="draw again", command=draw_again, bg="red")
+
+# display labels, check buttons and buttons in grid
+intro.grid(row=0, column=0, columnspan=3, padx=40, pady=20)
+choice.grid(row=1, column=0, columnspan=3, padx=40, pady=20)
 quadratic.grid(row=2, column=0, padx=10, pady=10)
 cubic.grid(row=2, column=1, padx=10, pady=10)
 sin.grid(row=2, column=2, padx=10, pady=10)
@@ -157,5 +217,6 @@ cosine.grid(row=3, column=0, padx=10, pady=10)
 log.grid(row=3, column=1, padx=10, pady=10)
 exponential.grid(row=3, column=2, padx=10, pady=10)
 draw_button.grid(row=4, column=0, padx=10, pady=10)
-again.grid(row=4, column=1, padx=10, pady=10)
+draw_again_button.grid(row=4, column=1, padx=10, pady=10)
+
 window.mainloop()
